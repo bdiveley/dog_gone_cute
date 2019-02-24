13971234233cb2d5f78df3312051a3eadc2daeb9
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-23 17:01:51 -0700

    Write test and pass for post request

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -2,4 +2,8 @@ class DogSerializer
   include FastJsonapi::ObjectSerializer
 
   attributes :id, :breed_id, :photo, :ave_score
+
+  attribute :ave_score do |object|
+    object.total_score / object.votes.to_f
+end
 end
