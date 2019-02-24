0d031ec9190bc76ee5dd8eaafd4ee40a43d6bec5
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-23 17:20:48 -0700

    Add request test to edit existing dog

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::DogsController < ApplicationController
     breed_name = params[:photo].split("/")[4]
     breed = Breed.where(name: breed_name)[0]
 
+    binding.pry
     dog = Dog.create(breed_id: breed.id, photo: params[:photo], votes: 1, total_score: params[:score])
 
     render json: DogSerializer.new(dog)
--- spec/requests/api/v1/dog_post_request_spec.rb
+++ spec/requests/api/v1/dog_post_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "POST requests for Dogs" do
-  it "POST /api/v1/dogs" do
+  it "POST /api/v1/dogs if dog does not yet exist" do
     breed = create(:breed, name: "deerhound-scottish")
     params = { photo: "https://images.dog.ceo/breeds/deerhound-scottish/n02092002_14567.jpg",
       score: 3
@@ -12,10 +12,34 @@ describe "POST requests for Dogs" do
     parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(Dog.last.photo).to eq("https://images.dog.ceo/breeds/deerhound-scottish/n02092002_14567.jpg")
+    expect(Dog.last.votes).to eq(1)
+    expect(Dog.last.total_score).to eq(3)
     expect(parsed[:data][:attributes]).to have_key(:id)
     expect(parsed[:data][:attributes]).to have_key(:breed_id)
     expect(parsed[:data][:attributes]).to have_key(:photo)
     expect(parsed[:data][:attributes]).to have_key(:ave_score)
     expect(parsed[:data][:attributes][:ave_score]).to eq(3.0)
+  end
+  it "POST /api/v1/dogs if dog already exists" do
+    breed = create(:breed, name: "deerhound-scottish")
+    dog = create(:dog, photo: "https://images.dog.ceo/breeds/deerhound-scottish/n02092002_14567.jpg", breed_id: breed.id, votes: 5, total_score: 20)
+    params = { photo: "https://images.dog.ceo/breeds/deerhound-scottish/n02092002_14567.jpg",
+      score: 1
+    }
+
+    post "/api/v1/dogs", params: params
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(Dog.count).to eq(1)
+    expect(dog.votes).to eq(2)
+    expect(dog.total_score).to eq(21)
+    expect(parsed[:data][:attributes]).to have_key(:id)
+    expect(parsed[:data][:attributes]).to have_key(:breed_id)
+    expect(parsed[:data][:attributes]).to have_key(:photo)
+    expect(parsed[:data][:attributes]).to have_key(:ave_score)
+    expect(parsed[:data][:attributes][:ave_score]).to eq(3.5)
   end
 end
