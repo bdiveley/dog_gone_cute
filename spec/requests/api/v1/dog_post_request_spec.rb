92c0ec304eff19c1d331517aeff3dfd64f1a7596
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-23 17:53:26 -0700

    Add helper methods to post controller method

--- spec/requests/api/v1/dog_post_request_spec.rb
+++ spec/requests/api/v1/dog_post_request_spec.rb
@@ -12,6 +12,7 @@ describe "POST requests for Dogs" do
     parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(Breed.count).to eq(1)
     expect(Dog.last.photo).to eq("https://images.dog.ceo/breeds/deerhound-scottish/n02092002_14567.jpg")
     expect(Dog.last.votes).to eq(1)
     expect(Dog.last.total_score).to eq(3)
@@ -33,9 +34,10 @@ describe "POST requests for Dogs" do
     parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(Breed.count).to eq(1)
     expect(Dog.count).to eq(1)
-    expect(dog.votes).to eq(2)
-    expect(dog.total_score).to eq(21)
+    expect(Dog.last.votes).to eq(6)
+    expect(Dog.last.total_score).to eq(21)
     expect(parsed[:data][:attributes]).to have_key(:id)
     expect(parsed[:data][:attributes]).to have_key(:breed_id)
     expect(parsed[:data][:attributes]).to have_key(:photo)
