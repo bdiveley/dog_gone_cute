533415c743c8588aa7c926a39e461b4f16cc071c
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-25 11:07:21 -0700

    Add test if no dogs in database

--- spec/requests/api/v1/dog_request_spec.rb
+++ spec/requests/api/v1/dog_request_spec.rb
@@ -2,6 +2,17 @@ require "rails_helper"
 
 describe "Requests for Dogs" do
   it "GET /api/v1/dogs?order=desc" do
+
+    get "/api/v1/dogs?order=desc"
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(parsed[:data]).to be_a(Array)
+    expect(parsed[:data].count).to eq(0)
+
+  end
+  it "GET /api/v1/dogs?order=desc" do
     dogs = create_list(:dog, 5)
 
     get "/api/v1/dogs?order=desc"
